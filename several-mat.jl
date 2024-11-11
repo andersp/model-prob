a69388b3ea2cd4a6f55a5b378b854066b282888e
Stefanie Guenther <guenther5@llnl.gov>
2024-11-11 13:29:52 -0800

    Adding residual for each window.

--- several-mat.jl
+++ several-mat.jl
@@ -22,16 +22,25 @@ function init_identity_unitary_mat!(d::Int64, Wvec::Vector{Matrix{ComplexF64}})
     end
 end
 
+function eval_trace_per_window(Zvec::Vector{Matrix{ComplexF64}}, Vvec::Vector{Matrix{ComplexF64}})
+    nlen = length(Zvec)
+    d = size(Zvec[1],1)
+
+    G = zeros(nlen)
+    G[1] = norm(Zvec[1])^2 - abs(tr(Zvec[1]'*Vvec[1]))^2/d 
+    for q=2:nlen
+        G[q] = norm(Zvec[q])^2 - abs(tr(Zvec[q]'*Vvec[q]*Zvec[q-1]))^2/d
+    end
+    return G
+end
+ 
 function eval_trace_obj(Zvec::Vector{Matrix{ComplexF64}}, Vvec::Vector{Matrix{ComplexF64}})
     # evaluate a sum of trace infidelities
     nlen = length(Zvec)
     d = size(Zvec[1],1)
 
-    G = norm(Zvec[1])^2 - abs(tr(Zvec[1]'*Vvec[1]))^2/d # first term
-    for q=2:nlen
-        G += norm(Zvec[q])^2 - abs(tr(Zvec[q]'*Vvec[q]*Zvec[q-1]))^2/d
-    end
-    return G/nlen
+    G = sum(eval_trace_per_window(Zvec, Vvec)) / nlen
+    return G
 end
 
 function trace_obj_line(t::Float64, Wvec::Vector{Matrix{ComplexF64}}, Hvec::Vector{Matrix{ComplexF64}}, Vvec::Vector{Matrix{ComplexF64}}, Uvec::Vector{Matrix{ComplexF64}})
@@ -82,163 +91,126 @@ Id = Matrix(I,d,d)
 # set the seed in the random number generator
 Random.seed!(1234)
 
-Nterms = 6 # Number of terms in the objective
+# Optimize with CG for given number of windows (Nterms) and maximum linesearch 'tmax'
+function runCG(Nterms, tmax1)
 
-Vvec = Vector{Matrix{ComplexF64}}(undef,Nterms)
-Wvec = Vector{Matrix{ComplexF64}}(undef,Nterms)
-Gvec = Vector{Matrix{ComplexF64}}(undef,Nterms)
-Svec = Vector{Matrix{ComplexF64}}(undef,Nterms)
-Nvec = Vector{Matrix{ComplexF64}}(undef,Nterms)
-Hvec = Vector{Matrix{ComplexF64}}(undef,Nterms)
-Uvec = Vector{Matrix{ComplexF64}}(undef,Nterms) # workspace
+    Vvec = Vector{Matrix{ComplexF64}}(undef,Nterms)
+    Wvec = Vector{Matrix{ComplexF64}}(undef,Nterms)
+    Gvec = Vector{Matrix{ComplexF64}}(undef,Nterms)
+    Svec = Vector{Matrix{ComplexF64}}(undef,Nterms)
+    Nvec = Vector{Matrix{ComplexF64}}(undef,Nterms)
+    Hvec = Vector{Matrix{ComplexF64}}(undef,Nterms)
+    Uvec = Vector{Matrix{ComplexF64}}(undef,Nterms) # workspace
 
-init_random_unitary_mat!(d, Vvec)
+    init_random_unitary_mat!(d, Vvec)
 
-# Initial guess: identity matrices
-init_identity_unitary_mat!(d, Wvec)
+    # Initial guess: identity matrices
+    init_identity_unitary_mat!(d, Wvec)
 
-# scaling (not used for CG)
-t₁_fact = 1.0
-t₂_fact = 1.0
-t₃_fact = 1.0
-
-# Euclidean gradient
-eval_Euclidean_grad!(Wvec, Vvec, Gvec)
-
-# Riemannian gradient (these functions are not currently used)
-# R₁(W₁,W₂) = skew(∇₁G(W₁,W₂)*W₁') * W₁
-# R₂(W₁,W₂,W₃) = skew(∇₂G(W₁,W₂,W₃)*W₂') * W₂
-# R₃(W₂,W₃) = skew(∇₃G(W₂,W₃)*W₃') * W₃
-
-# skew-Hermitian matrices from the Euclidian gradient
-for q=1:Nterms
-    Svec[q] = skew(Gvec[q]*Wvec[q]')
-end
-
-# The initial search direction equals S
-γ = 0.0
-Hvec[:] = Svec[:]
-
-tmax_x = 3.0
-tmax_y = 3.0
-tmax_z = 3.0
-t₁ = LinRange(-tmax_x, tmax_x, 201)
-t₂ = LinRange(-tmax_y, tmax_y, 201)
-t₃ = LinRange(-tmax_z, tmax_z, 201)
-
-t₀ = 0.0
-str_1 = @sprintf("t₁")
-str_2 = @sprintf("t₂")
-str_3 = @sprintf("t₃")
-
-# ofunc(t₁,t₂) =  trace_obj(U₁(t₁), U₂(t₂), U₃(t₀)) # trace objective function, unitary retraction
-# plc_12 = Plots.contourf(t₁_fact*t₁, t₂_fact*t₂, ofunc.(t₁,t₂'), color=:tofino, xaxis=str_1, yaxis=str_2, title="Trace objective, three terms")
-# scatter!(plc_12, [0.0], [0.0], lab="Origin")
-# println("Cartesian contour plot in variable 'plc_12'")
-
-# ofunc_13(t₁,t₃) =  trace_obj(U₁(t₁), U₂(t₀), U₃(t₃)) # trace objective function, unitary retraction
-# plc_13 = Plots.contourf(t₁_fact*t₁, t₃_fact*t₃, ofunc_13.(t₁,t₃'), color=:tofino, xaxis=str_1, yaxis=str_3, title="Trace objective, three terms")
-# scatter!(plc_13, [0.0], [0.0], lab="Origin")
-# println("Cartesian contour plot in variable 'plc_13'")
-
-# ofunc_23(t₂,t₃) =  trace_obj(U₁(t₀), U₂(t₂), U₃(t₃)) # trace objective function, unitary retraction
-# plc_23 = Plots.contourf(t₂_fact*t₂, t₃_fact*t₃, ofunc_23.(t₂,t₃'), color=:tofino, xaxis=str_2, yaxis=str_3, title="Trace objective, three terms")
-# scatter!(plc_23, [0.0], [0.0], lab="Origin")
-# println("Cartesian contour plot in variable 'plc_23'")
-
-# Complex extrapolation
-# ofunc2(t₁,t₂) =  trace_obj(Z₁(t₁), Z₂(t₂), Z₃(t₀)) # trace objective function, Euclidian gradient
-# plh = Plots.contourf(t₁_fact*t₁, t₂_fact*t₂, ofunc2.(t₁,t₂'), color=:tofino, xaxis=str_1, yaxis=str_2, title="Trace objective, complex extrapolation")
-# scatter!(plh, [0.0], [0.0], lab="Origin", leg=:left)
-# println("Contour plot in variable 'plh'")
-
-tmax1 = 2.5 # max range for line search
-tls = LinRange(0.0, tmax1, 101)
-
-tstr= @sprintf("Opt iterations, t₁_s = %5.2f, t₂_s = %5.2f, t₃_s = %5.2f", t₁_fact, t₂_fact, t₃_fact)
-plo = plot(title=tstr, leg=:outerright, size=(800,400), xaxis = "t", yaxis = "G(U₁(-t),U₂(-t))", ylims=(0,4.0))
-
-ofunc3(t) =  trace_obj_line(-t, Wvec, Hvec, Vvec, Uvec) # objective function, fixed direction
-
-tstr = "Trace objective, unitary retraction"
-
-to_init = trace_obj_line(0.0, Wvec, Hvec, Vvec, Uvec)
-println("initial guess, objective (G) = ", to_init, " t=0.0")
-
-max_iter = 100
-obj_hist = zeros(max_iter)
-Niter = 0
-# steepest descent with line-search
-for iter = 0:max_iter-1
-    global obj_hist, γ, Niter
-
-    Niter = iter
-    Gkk = inner_prod(Svec, Svec)
-
-    if Gkk < 1e-9
-        println("Found local minima after ", Niter, " CG iterations, Gkk = ", Gkk)
-        break
-    end
-
-    op2 = ofunc3.(tls) # Follow the unitary retraction (for plotting)
-
-    # line search
-    result = optimize(ofunc3, -tmax1, tmax1, GoldenSection())
-
-    t_min = Optim.minimizer(result)
-    o_min = Optim.minimum(result)
-    obj_hist[iter+1] = o_min
-
-    println("iter = ", iter, " Gkk = ", Gkk, " γ = ", γ, ", minimum (G) = ", o_min, " minimizer (t): ", t_min)
-
-    if iter <10
-        lstr = "uni-"*string(iter)
-        plot!(plo, tls, op2, lab=lstr, lw=3)
-        scatter!(plo, [t_min], [o_min], lab=:false)
-    end
-
-    # next W:
-    unitary_retraction!(-t_min, Wvec, Hvec, Uvec)
-    Wvec[:] = Uvec[:]
-
+    # Euclidean gradient
     eval_Euclidean_grad!(Wvec, Vvec, Gvec)
+
     # skew-Hermitian matrices from the Euclidian gradient
     for q=1:Nterms
-        Nvec[q] = skew(Gvec[q]*Wvec[q]')
+        Svec[q] = skew(Gvec[q]*Wvec[q]')
     end
 
-    Uvec[:] = Nvec[:] - Svec[:]
-    γ = inner_prod(Uvec, Nvec)/Gkk
+    # The initial search direction equals S
+    global γ = 0.0
+    Hvec[:] = Svec[:]
 
-    # next search direction
-    Hvec[:] = Nvec[:] + γ .* Hvec[:]
+    # Initial objective funciton value
+    to_init = trace_obj_line(0.0, Wvec, Hvec, Vvec, Uvec)
+    println("initial guess, objective (G) = ", to_init, " t=0.0")
 
-    HGprod = inner_prod(Hvec, Nvec)
-    if HGprod < 0.0
-        println("HGprod = ", HGprod, " < 0, taking a steepest descent direction")
-        Hvec[:] = Nvec[:]
+    # Define line search objective function with fixed direction
+    ofunc3(t) =  trace_obj_line(-t, Wvec, Hvec, Vvec, Uvec) 
+
+    max_iter = 200
+    global obj_hist = zeros(max_iter)
+    global residuals = zeros(Nterms, max_iter)
+    global Niter = 0
+
+    # CG iterations with line-search
+    for iter = 0:max_iter-1
+
+        Niter = iter
+        Gkk = inner_prod(Svec, Svec)
+
+        if Gkk < 1e-9
+            println("Found local minima after ", Niter, " CG iterations, Gkk = ", Gkk)
+            break
+        end
+
+        # line search
+        result = optimize(ofunc3, -tmax1, tmax1, GoldenSection())
+
+        t_min = Optim.minimizer(result)
+        o_min = Optim.minimum(result)
+        obj_hist[iter+1] = o_min
+
+        println("iter = ", iter, " Gkk = ", Gkk, " γ = ", γ, ", minimum (G) = ", o_min, " minimizer (t): ", t_min)
+
+        # next W:
+        unitary_retraction!(-t_min, Wvec, Hvec, Uvec)
+        Wvec[:] = Uvec[:]
+
+        # Store residuals per window
+        residuals[:, iter+1]  = eval_trace_per_window(Wvec, Vvec)
+
+        eval_Euclidean_grad!(Wvec, Vvec, Gvec)
+        # skew-Hermitian matrices from the Euclidian gradient
+        for q=1:Nterms
+            Nvec[q] = skew(Gvec[q]*Wvec[q]')
+        end
+
+        Uvec[:] = Nvec[:] - Svec[:]
+        γ = inner_prod(Uvec, Nvec)/Gkk
+
+        # next search direction
+        Hvec[:] = Nvec[:] + γ .* Hvec[:]
+
+        HGprod = inner_prod(Hvec, Nvec)
+        if HGprod < 0.0
+            println("HGprod = ", HGprod, " < 0, taking a steepest descent direction")
+            Hvec[:] = Nvec[:]
+        end
+
+        # save new descent direction
+        Svec[:] = Nvec[:]
     end
 
-    # save new descent direction
-    Svec[:] = Nvec[:]
+    return obj_hist, residuals[:,1:Niter]
+end # of function runCG
+
+
+##########
+# Run CG on increasing numbers of windows
+##########
+
+# Number of windows in the objective
+# Nterms_all = [2,3,4,5,6]
+Nterms_all = [2,4,8,16,32,64]
+tmax1_all = Nterms_all
+
+# Run CG for each number of windows
+objhist = []
+resids = []
+for i=1:length(Nterms_all)
+    hist, res = runCG(Nterms_all[i], tmax1_all[i])
+    push!(objhist, hist)
+    push!(resids, res)
 end
-println("Line search plot object stored in variable 'plo'")
 
-tstr = @sprintf("CG conv hist")
-plconv = plot(obj_hist[1:Niter], lab=string(Nterms)*"-terms", title=tstr, yaxis=:log10,ylims=(1e-10,1e1), xlabel="Iteration", ylabel="Objective")
-println("Convergence plot in variable 'plconv'")
+# Plot convergence for each number of windows
+i=1
+pl_objhist = plot(objhist[i], yaxis=:log10, ylims=(1e-10,1e1), xlims=(1,Niter), xlabel="Iteration", ylabel="Objective", label="Nterms="*string(Nterms_all[i]))
+for i=2:length(objhist)
+    global pl_objhist = plot!(objhist[i], yaxis=:log10, ylims=(1e-10,1e1), xlims=(1,Niter), xlabel="Iteration", ylabel="Objective", label="Nterms="*string(Nterms_all[i]))
+end
+println("\n All convergence histories plotted in 'pl_objhist'.")
 
-# plot landscape near the local optima
-
-# plf_12 = Plots.contourf(t₁_fact*t₁, t₂_fact*t₂, ofunc.(t₁,t₂'), color=:tofino, xaxis=str_1, yaxis=str_2, title="Trace objective, three terms")
-# scatter!(plf_12, [0.0], [0.0], lab="Origin")
-# println("Cartesian contour plot in variable 'plf_12'")
-
-# plf_13 = Plots.contourf(t₁_fact*t₁, t₃_fact*t₃, ofunc_13.(t₁,t₃'), color=:tofino, xaxis=str_1, yaxis=str_3, title="Trace objective, three terms")
-# scatter!(plf_13, [0.0], [0.0], lab="Origin")
-# println("Cartesian contour plot in variable 'plf_13'")
-
-# plf_23 = Plots.contourf(t₂_fact*t₂, t₃_fact*t₃, ofunc_23.(t₂,t₃'), color=:tofino, xaxis=str_2, yaxis=str_3, title="Trace objective, three terms")
-# scatter!(plf_23, [0.0], [0.0], lab="Origin")
-# println("Cartesian contour plot in variable 'plf_23'")
-
+# For the largest number of Nterms, plot the residuals per window, per iteration
+plotniters = 50
+pl_residual = plot(resids[end][:,1:plotniters], yscale=:log10, legend=false, xlabel="window", ylabel="residual")
+println(" Residuals per window for Nterms=", Nterms_all[end], " plotted in 'pl_residual'.")
